5e20b14ac94fddd0fe03051e61e8643a5eabfedc
Sasuke-7 <davidkunschventurini@gmail.com>
2022-03-10 21:45:43 -0300

    add og meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,9 +32,28 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt">
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Open+Sans:wght@300;400;500;700&family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Open+Sans:wght@300;400;500;700&family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://yunestore.xyz" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="og:site_name" content="Yune Store" />
+          <meta
+            property="og:keywords"
+            content="Discord,Yune,Yune Store,Yune Bots,Discord Bot,Bots Discord,Bot de ranqueada, Bot de apostado"
+          />
+          <meta property="og:title" content="Yune Store | Os melhores bots ranqueado e apostado para seu servidor!" />
+          <meta
+            property="og:description"
+            content="Yune é uma loja de bots para o Discord que oferece o melhor e mais moderno bot de ranqueada e apostado do mercado."
+          />
+          <meta name="theme-color" content="#0084FF" />
+          <meta data-react-helmet="true" charSet="utf-8" />
+          <meta data-react-helmet="true" property="og:image" content="/assets/adversiment.png" />
+          <meta data-react-helmet="true" property="twitter:image" content="/assets/adversiment.png" />
         </Head>
 
         <body>
