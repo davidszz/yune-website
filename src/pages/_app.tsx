80cfd45dd0056acd6ea629160957d7b62becd59a
Sasuke-7 <davidkunschventurini@gmail.com>
2022-03-15 16:31:32 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app'
 
-import { GlobalStyles } from '@styles/global';
-import { NProgress } from '@components/NProgress';
 import { AuthProvider } from '@contexts/AuthContext';
+
+import { NProgress } from '@components/NProgress';
+
+import { GlobalStyles } from '@styles/global';
 
 function App({ Component, pageProps }: AppProps) {
   return (
