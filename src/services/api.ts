b7a2e514cf9c94b5746c91dec1a990d06989aee7
Sasuke-7 <davidkunschventurini@gmail.com>
2022-03-15 16:42:46 -0300

    fix: api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env);
-
 export const api = axios.create({
-  baseURL: `${process.env.BASE_URL ?? 'https://yunestore.xyz'}/api`,
+  baseURL: '/api'
 });
